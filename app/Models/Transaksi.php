1598bc6a3b0589949a346ae666dfe90356f2ecec
RillGoodd <141595790+RillGoodd@users.noreply.github.com>
2024-03-27 10:52:56 +0700

    update model transaksi

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Traits\HasFormatRupiah;
 
 class Transaksi extends Model
 {
     use HasFactory;
+    use HasFormatRupiah;
+
+    protected $fillable = [
+        'kode_transaksi',
+        'total',
+        'bayar',
+        'kembali',
+        'kode_kasir',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
